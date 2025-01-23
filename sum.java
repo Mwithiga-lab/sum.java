9cb35eac85b50d7020839bde6eaf3284fb8acaed
Mwithiga-lab <128584553+Mwithiga-lab@users.noreply.github.com>
2025-01-23 22:22:11 +0300

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,18 @@
+/*
+Program to display the sum of 5 and 7
+Author: Kevin Mwithiga
+Reg: CT101/G/19870/23
+Date: 22/1/2025
+*/
+//sum class
+public class sum
+{
+// main Method
+public static void main(String[] args)
+{
+int x = 5;
+int y = 7;
+int sum = x+y;
+System.out.println("Sum: " + sum);
+}
+}
